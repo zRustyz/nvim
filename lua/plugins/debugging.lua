7c6067848bcd2e8e3f2dc573721aa05ce40d7242
zRustyz <Russell.liu05@gmail.com>
2024-04-21 18:54:58 -0700

    added modules for py, ts, and js debugging

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,73 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"mfussenegger/nvim-dap-python",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		require("dapui").setup()
+		require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+
+		require("dap").adapters["pwa-node"] = {
+			type = "server",
+			host = "localhost",
+			port = "${port}",
+			executable = {
+				command = "node",
+				-- 💀 Make sure to update this path to point to your installation
+				args = { "~/.virtualenvs/js-debug/src/dapDebugServer.js", "${port}" },
+			},
+		}
+
+		dap.configurations.typescript = {
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch file",
+				runtimeExecutable = "deno",
+				runtimeArgs = {
+					"run",
+					"--inspect-wait",
+					"--allow-all",
+				},
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				attachSimplePort = 9229,
+			},
+		}
+		dap.configurations.javascript = {
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch file",
+				runtimeExecutable = "deno",
+				runtimeArgs = {
+					"run",
+					"--inspect-wait",
+					"--allow-all",
+				},
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				attachSimplePort = 9229,
+			},
+		}
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
+}
